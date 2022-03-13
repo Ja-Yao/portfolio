18438ce95d1ae355486482dd7b83b83617903b2f
jacob.yao <jacob@blackcape.io>
2022-03-12 23:21:47 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#D05353',
+      contrastText: '#131318'
+    }
+  },
+  typography: {
+    fontFamily: 'Poppins',
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
