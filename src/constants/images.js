a2d7458846f5b2874dbfb2cb4c688cfb2722fd44
jacob.yao <jacob@blackcape.io>
2022-03-12 23:23:35 -0500

    Setup directory structure

---
+++ src/constants/images.js
@@ -0,0 +1,34 @@
+import api from '../assets/api.png';
+import css from '../assets/css.png';
+import figma from '../assets/figma.png';
+import git from '../assets/git.png';
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import mu5 from '../assets/mu5.png';
+import node from '../assets/node.png';
+import react from '../assets/react.png';
+import redux from '../assets/redux.png';
+import sass from '../assets/sass.png';
+import UX from '../assets/UX.jpg';
+import frontend from '../assets/frontend.jpg'
+
+import portrait from '../assets/portrait.jpg';
+import circle from '../assets/circle.svg';
+
+export default {
+  api,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  mu5,
+  node,
+  react,
+  redux,
+  sass,
+  portrait,
+  circle,
+  UX,
+  frontend,
+};
