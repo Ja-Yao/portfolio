12a3cb8f0eeade1539f40833aa95d84f83a53439
Ja-Yao <jtyaos@gmail.com>
2022-03-16 23:06:41 -0400

    Removed Unnecessary code

--- src/wrapper/AppWrap.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { NavDots, Socials } from '../components';
-
-const AppWrap = (Component, idName, classNames) => function HOC() {
-  return (
-    <>
-    </>
-  )
-}
